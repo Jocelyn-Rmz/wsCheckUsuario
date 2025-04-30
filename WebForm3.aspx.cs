bbba4d458f806b6f59165bb84fbbb1e73e3ab7d9
Jocelyn-Rmz <ramirezjocelyn205@gmail.com>
2025-04-30 00:10:30 -0600

    Versión Inicial

---
+++ WebForm3.aspx.cs
@@ -0,0 +1,168 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using wsCheckUsuario.Models;
+
+namespace wsCheckUsuario
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected async void Page_Load(object sender, EventArgs e)
+        {
+            //Llamada patra ejecucion del metodo 
+            await cargaDatosTipoUsuario();
+        }
+        // Creación del método asíncrono para ejecutar el
+        // endpoint vwTipoUsuario
+        private async Task cargaDatosTipoUsuario()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Configuración de la peticion HTTP
+                    string apiUrl = "https://localhost:44335/check/usuario/vwtipousuario";
+                    // Ejecución del endpoint
+                    HttpResponseMessage respuesta = await client.GetAsync(apiUrl);
+                    // ---------------------------------------------------
+                    // Validación de recepción de respuesta Json
+                    clsApiStatus objRespuesta = new clsApiStatus();
+
+                    // Validación del estatus OK
+                    if (respuesta.IsSuccessStatusCode)
+                    {
+                        string resultado = await respuesta.Content.ReadAsStringAsync();
+                        objRespuesta = JsonConvert.DeserializeObject<clsApiStatus>(resultado);
+                        // ------------------------------------------
+                        JArray jsonArray = (JArray)objRespuesta.datos["vwTipoUsuario"];
+                        // Convertir JArray a DataTable
+                        DataTable dt = JsonConvert.DeserializeObject<DataTable>(jsonArray.ToString());
+                        // -------------------------------------------
+                        // Visualización de los datos formateados DropDownList
+                        DropDownList1.DataSource = dt;
+                        DropDownList1.DataTextField = "descripcion";
+                        DropDownList1.DataValueField = "clave";
+                        DropDownList1.DataBind();
+                    }
+                    else
+                    {
+                        Response.Write("<script language='javascript'>" +
+                                       "alert('Error de conexión con el servicio');" +
+                                       "</script>");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script language='javascript'>" +
+                               "alert('Error de la aplicación, intentar nuevamente');" +
+                               "</script>");
+            }
+        }
+
+        // Creación del método asíncrono para ejecutar el
+        // endpoint spInsUsuario
+        private async Task cargaDatos()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Configuración del Json que se enviará
+                    String data = @"{
+                                  ""nombre"":""" + TextBox2.Text + "\"," +
+                                  "\"apellidoPaterno\":\"" + TextBox3.Text + "\"," +
+                                  "\"apellidoMaterno\":\"" + TextBox4.Text + "\"," +
+                                  "\"usuario\":\"" + TextBox5.Text + "\"," +
+                                  "\"contrasena\":\"" + TextBox6.Text + "\"," +
+                                  "\"ruta\":\"" + TextBox7.Text + "\"," +
+                                  "\"tipo\":\"" + DropDownList1.SelectedValue + "\"" +
+                                  "}";
+                    // Configuración del contenido del <body> a enviar
+                    HttpContent contenido = new StringContent
+                                (data, Encoding.UTF8, "application/json");
+                    // Ejecución de la petición HTTP
+                    string apiUrl = "https://localhost:44335/check/usuario/spinsusuario";
+                    // ----------------------------------------------
+                    HttpResponseMessage respuesta =
+                        await client.PostAsync(apiUrl, contenido);
+                    // ---------------------------------------------------
+                    // Validación de recepción de respuesta Json
+                    clsApiStatus objRespuesta = new clsApiStatus();
+                    // ---------------------------------------------------
+
+                    if (respuesta.IsSuccessStatusCode)
+                    {
+                        string resultado =
+                                await respuesta.Content.ReadAsStringAsync();
+                        objRespuesta = JsonConvert.DeserializeObject<clsApiStatus>(resultado);
+
+                        // Bandera de estatus del proceso
+                        if (objRespuesta.ban == 0)
+                        {
+                            Response.Write("<script language='javascript'>" +
+                                           "alert('Usuario registrado exitosamente');" +
+                                           "</script>");
+                            Response.Write("<script language='javascript'>" +
+                                           "document.location.href='WebForm2.aspx';" +
+                                           "</script>");
+                        }
+                        if (objRespuesta.ban == 1)
+                        {
+                            Response.Write("<script language='javascript'>" +
+                                           "alert('El nombre de usuario ya existe');" +
+                                           "</script>");
+                        }
+                        if (objRespuesta.ban == 2)
+                        {
+                            Response.Write("<script language='javascript'>" +
+                                           "alert('El usuario ya existe');" +
+                                           "</script>");
+                        }
+                        if (objRespuesta.ban == 3)
+                        {
+                            Response.Write("<script language='javascript'>" +
+                                           "alert('El tipo de usuario no existe');" +
+                                           "</script>");
+                        }
+                    }
+                    else
+                    {
+                        Response.Write("<script language='javascript'>" +
+                                       "alert('Error de conexión con el servicio');" +
+                                       "</script>");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script language='javascript'>" +
+                               "alert('Error de la aplicación, intentar nuevamente');" +
+                               "</script>");
+            }
+        }
+
+
+
+
+        protected async void Button1_Click(object sender, EventArgs e)
+        {
+            //Ejecucion asincrona del metodo de insercion de usuario
+            await cargaDatos();
+        }
+
+        protected void TextBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
